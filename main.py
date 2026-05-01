471399ecae33537ed053af81b6f0d2ec406c7753
Briant <briant.a.cardozo@gmail.com>
2026-04-30 23:11:26 -0300

    primer intento de actualizar la pagina con el tiempo

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import flet as ft
+import time
+import asyncio
+
 from backend.cropsystem.crops import Crop
 
+wheat = Crop("Trigo", 5, 10)
+
 def main(page: ft.Page):
-    wheat = Crop("Trigo", 5, 10)
+
+    def plant(e):
+        wheat.plant()
+
+    def harvest(e):
+        wheat.harvest()
 
     page.add(
-         ft.Button(
-            content="Mi botón",
-            on_click=wheat.plant()
-        )
+        ft.Button(content="Cosechar", on_click=harvest),
+        ft.Button(content="Plantar", on_click=plant)
     )
 
 ft.app(target=main)
