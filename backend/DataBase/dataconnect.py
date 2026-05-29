1149f977a57376b4be23e6ea315fa5a01a0926dc
Briant <briant.a.cardozo@gmail.com>
2026-05-29 14:56:18 -0300

    Actualizando la base de datos para agregar/editar/eliminar jugadores

--- backend/DataBase/dataconnect.py
+++ backend/DataBase/dataconnect.py
@@ -1,4 +1,5 @@
 import sqlite3 as db
+from ssl import VERIFY_ALLOW_PROXY_CERTS
 
 
 def createDB():
@@ -6,13 +7,12 @@ def createDB():
     conn.commit()
     conn.close()
 
-def createTable():
+def createTable(nombreTabla):
     conn = db.connect("players.db")
-    cursor = conn.cursor()
     conn.execute(
-        """CREATE TABLE player(
+        f"""CREATE TABLE IF NOT EXISTS {nombreTabla}(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE,
+        name TEXT NOT NULL UNIQUE CHECK(length(name) <= 12),
         money INTEGER DEFAULT 0,
         level INTEGER DEFAULT 1
         )"""
@@ -21,7 +21,64 @@ def createTable():
     conn.close()
 
 
+    #------------AGREGA EL NOMBRE DEL JUGADOR A LA LISTA DE JUGADORES---------------------
+def insertrow(nombreTabla, name):
+    conn = db.connect("players.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        f"""
+        INSERT INTO {nombreTabla} (name)
+        VALUES (?)
+        """,
+        (name,)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def editnameplayer(id, newname):
+    conn = db.connect("players.db")
+    cursor = conn.cursor()
+    conn.execute(
+        f"""
+            UPDATE player
+            SET name = ?
+            WHERE id = ?
+        """,
+        (newname, id)
+    )
+    conn.commit()
+    conn.close
+#-------------BORRA UNA TABLA DE LA BASE DE DATOS-------------
+def deletetable(nombreTabla):
+    conn = db.connect("players.db")
+    cursor = conn.cursor()
+    conn.execute(
+        f"""
+        DROP TABLE {nombreTabla};"""
+    )
+    conn.commit()
+    conn.close()
+#-----------ELIMINAR CUENTA DE JUGADOR-----------------
+def deleteplayer(id, playername):
+    conn = db.connect("players.db")
+    cursor = conn.cursor()
+    conn.execute(
+        f"""
+        DELETE FROM player WHERE id == {id} AND name == '{playername}'"""
+    )
+    conn.commit()
+    conn.close()
+
+
+
 
 if __name__ == "__main__":
     #createDB()
-    createTable()
+    #deletetable("player")
+    #createTable("player")
+    #insertrow("player", "Daniel")
+    #deleteplayer(1, "Daniel")
+    editnameplayer(1, "Terrible_1pqipwdiasd2")
